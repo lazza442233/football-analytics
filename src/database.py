629be6ca57908049cedd3d8bb290bb852bdbf773
Lennon <lennon.abonyi@gmail.com>
2026-01-31 13:10:09 +1100

    feat: add Alembic configuration files and migration scripts for database management

--- src/database.py
+++ src/database.py
@@ -1,3 +1,4 @@
+from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import sessionmaker
@@ -8,7 +9,7 @@ from src.config import settings
 engine = create_async_engine(settings.DATABASE_URL, echo=True, future=True)
 
 
-async def get_session() -> AsyncSession:
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async_session = sessionmaker(
         engine, class_=AsyncSession, expire_on_commit=False
     )
